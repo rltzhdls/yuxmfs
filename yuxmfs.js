efc75f2d33dae42e966d91d6aa02d1a70850e4b3
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-28 12:29:29 +0900

    Add files via upload

---
+++ yuxmfs.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/fmfkds@main/s4.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
