150330f3b78b9673c0639514a7aeb51658ea5f3e
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2025-02-10 16:45:06 +0900

    Update yuxmfs.js

--- yuxmfs.js
+++ yuxmfs.js
@@ -5,5 +5,10 @@ $(document).ready(function () {
         vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/fmfkds@main/s4.mp4";
         document.getElementById('vd').load();
     }
+
+    if (!window.location.href.startsWith("https://l1sssv.blogspot.com/")) {
+    window.location.href = "http://warning.or.kr/i12.html";
+}
+
 });
 
