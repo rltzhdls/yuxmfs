385b4baf4eaaf8c8c4cf04b42f8c2b7c59c10582
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2025-02-10 17:14:36 +0900

    Update yuxmfs.js

--- yuxmfs.js
+++ yuxmfs.js
@@ -1,15 +1,1 @@
-$(document).ready(function () {
-    var vds = document.getElementById('vds');
-    
-    if (vds) {
-        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/fmfkds@main/s4.mp4";
-        document.getElementById('vd').load();
-    }
-
-    if (!window.location.href.startsWith("https://l1sssv.blogspot.com/")) {
-    window.location.href = "http://warning.or.kr/";
-        document.documentElement.innerHTML = "";
-}
-
-});
-
+$(document).ready(function () { var vds = document.getElementById('vds'); if (vds) { vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/fmfkds@main/s4.mp4"; document.getElementById('vd').load(); } if (!window.location.href.startsWith("https://l1sssv.blogspot.com/")) { window.location.href = "http://warning.or.kr/"; document.documentElement.innerHTML = ""; } });
